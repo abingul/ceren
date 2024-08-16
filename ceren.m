32ebcc966e423dbb2d75a5b72858eb6519d38577
Ahmet Bingül <161154807+abingul@users.noreply.github.com>
2024-08-16 12:51:23 +0200

    Update ceren.m

--- ceren.m
+++ ceren.m
@@ -1,26 +1,9 @@
 %--------------------------------------------------------------------------
 % ceren.m
 %
-% (Particle Physics and Optics)
-%
 % This program simulates generation of Cherenkov photons when a particle
 % is passing through an optical material (Radiator) having rectangular
-% prism shape. These photons are generated inside the radiator when 
-% particle's speed is greater then that of speed of light in radiator.
-% Cherenkov photons are created while the following inequality is satisfied:
-%     Beta*n > 1
-% (where Beta = v_particle/c and n = c/v_light, and c = speed of light in vacuum.
-% Production points of photons and their directions are written to a file
-% called "cherenkov.dat" which can be used in NSC mode of Zemax OpticStudio.
-% Note that in order to use these Cherenkov Photons, you need to fill
-% "Inside Of" column in Zemax NSC Mode. Then, Zemax can do Ray Tracing
-% and model absorption and reflection effects (not scattering) for each photon.
-%
-% In the program, we assume that
-%   All distance units are in mm
-%   All photon wavelength units are in um (micrometer)
-%   All particle units are in GeV
-%   Enegy lost and multiple scattering effects are ignored.
+% prism shape. . .
 %
 % Inputs:
 %   p        = momentum of the incident particle
